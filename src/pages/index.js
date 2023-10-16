fb3454605550e4c67406e254ad51fa2ef2613c60
Ryan <rc1507a@student.american.edu>
2023-10-16 16:51:35 -0400

    Index Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import styles from "../styles/index/Index.module.scss";
 
 const Index = () => {
   return(
-    <>
-      <h1>Index</h1>
-    </>
+      <body className={styles['back']}>
+        <div className={styles["background"]}>
+          <h1 className={styles["firstLine"]}>Hello, My Name Is Ryan!</h1>
+        </div>
+          
+      </body>
   )
 }
 
---
+++ src/pages/resume.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Resume = () => {
+    return(
+        <>
+            <h1>helloworld</h1>
+        </>
+    )
+}
+
+export default Resume;
