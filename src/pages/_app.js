d7b40857331c43db76c54ce6004d6186e8e5aadc
Ryan <rc1507a@student.american.edu>
2023-10-16 17:07:49 -0400

    Intial Bootstrap Nav Added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from "../components/shared/navigation/Navigation";
+import { Container } from 'react-bootstrap';
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navigation />
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    </>
+  );
 }
