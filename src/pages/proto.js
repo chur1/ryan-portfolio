6b799d073201561e8f339a81ba4b22a0781859a5
Ryan <rc1507a@student.american.edu>
2023-11-01 18:03:32 -0400

    background responsiveness fixed

--- src/pages/proto.js
+++ src/pages/proto.js
@@ -2,7 +2,30 @@ import React, { useEffect } from "react";
 import styles from "../styles/proto/Proto.module.scss";
 import Navigation from "../components/shared/navigation/Navigation";
 
+function setScrollVar() {
+    const htmlElement = document.documentElement;
+    const percentOfScreenHeightScrolled = htmlElement.scrollTop / htmlElement.clientHeight;
+    console.log(Math.min(percentOfScreenHeightScrolled));
+    htmlElement.style.setProperty("--scroll", `${Math.min(percentOfScreenHeightScrolled * 100, 100)}%`);
+}
+
+
 const Proto = () => {
+
+    useEffect(() => {
+        // Add scroll and resize event listeners
+        window.addEventListener("scroll", setScrollVar);
+        window.addEventListener("resize", setScrollVar);
+    
+        // Initial call to setScrollVar to set the initial value
+        setScrollVar();
+    
+        // Cleanup the event listeners when the component unmounts
+        return () => {
+        window.removeEventListener("scroll", setScrollVar);
+        window.removeEventListener("resize", setScrollVar);
+        };
+    }, []);
 
 return (
     <>
@@ -12,11 +35,11 @@ return (
             </div>
             <h1 className={styles['hello_text']}>Hello, My Name is Ryan _</h1>
         </section>
-        {/* <section>
+        <section>
             <div>
-                <h1>hello</h1>
+                <h1 className={styles['hello']}>hello</h1>
             </div>
-        </section> */}
+        </section>
     </>
 );
 }
