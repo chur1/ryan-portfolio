1431033f80cb1b574f379095b0d7ed9fef078b55
Ryan <rc1507a@student.american.edu>
2023-11-08 23:13:38 -0500

    notification experience page implemented v1

--- src/components/title-description/ImageDescription.js
+++ src/components/title-description/ImageDescription.js
@@ -4,23 +4,33 @@ import styles from "./ImageDescription.module.scss";
 
 const ImageDescription = ({   
     orientation, 
+    title,
     image,
-    text
+    date,
+    text,
 }) => {
     if(orientation == "left"){
         return(
             <div className={styles['left-whole-container']}>
-                <div className={styles['left-image']}>
-                    <Image
-                        className={styles['Image']}
-                        alt="image"
-                        src={image}
-                        width={200}
-                        height={200}
-                    />
-                </div>
-                <div className={styles['left-description']}>
-                    <h4 className={styles['left-text']}>{text}</h4>
+                <div className={styles['container-contents']}>
+                    <div className={styles['left-image']}>
+                        <div className={styles['imageTitle']}>
+                            <Image
+                                className={styles['Image']}
+                                alt="image"
+                                src={image}
+                                width={50}
+                                height={50}
+                            />
+                            <h3 className={styles['title']}>{title}</h3>
+                        </div>
+                        <div>
+                            <h4 className={styles['date']}>{date}</h4>
+                        </div>
+                    </div>
+                    <div className={styles['left-description']}>
+                        <h5 className={styles['left-text']}>{text}</h5>
+                    </div>
                 </div>
             </div>
         )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import styles from "../styles/index/index.module.scss";
-import Navigation from "../components/shared/navigation/Navigation";
+import ImageDescription from "@/components/title-description/ImageDescription";
 
 function setScrollVar() {
     const htmlElement = document.documentElement;
@@ -12,6 +12,7 @@ function setScrollVar() {
 
 const Proto = () => {
 
+    // scroll effect 0-1
     useEffect(() => {
         // Add scroll and resize event listeners
         window.addEventListener("scroll", setScrollVar);
@@ -27,19 +28,60 @@ const Proto = () => {
         };
     }, []);
 
+    // image cards defined
+    const ManExpeditions = {
+        orientation: "left",
+        title: "Man Expeditions",
+        image: "https://i.ibb.co/Dkcvj1J/IMG-6221.jpg",
+        date: "Aug 2020 - Sep 2020",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+    }
+      
+    const SkyIT = {
+        orientation: "left",
+        title: "SkyIT",
+        image: "https://i.ibb.co/Dkcvj1J/IMG-6221.jpg",
+        date: "Mar 2023 - Sep 2023",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+    }
+    const DOE = {
+        orientation: "left",
+        image: "https://i.ibb.co/Dkcvj1J/IMG-6221.jpg",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+    }
+
 return (
     <>
         <div className={styles['page-container']}>
+
+            {/* background video */}
+            <div className={styles['backgroundVideo']}>
+                <video className={styles['jacko']} id="backgroundVideo" src={"/videos/Sequence_01.mp4"} autoPlay muted loop playsInline />
+            </div>
+
+            {/* first welcome page */}
             <section className={styles['first-page']}>
-                <div className={styles['backgroundVideo']}>
-                    <video className={styles['jacko']} id="backgroundVideo" src={"/videos/Sequence_01.mp4"} autoPlay muted loop playsInline />
-                </div>
                 <h1 className={styles['hello_text']}>Hello, My Name is Ryan!</h1>
                 <h5 className={styles['scroll-down-text']}>\/</h5>
             </section>
+
+            {/* second experience page */}
             <section className={styles['second-page']}>
                 <div>
-                    <h1 className={styles['hello']}>Please Check Back Later :(</h1>
+                    <div className={styles['imageCards']}>
+                        <div>
+                            <ImageDescription {...ManExpeditions} priority={true} />
+                        </div>
+                        <div>
+                            <ImageDescription {...SkyIT} priority={true} />
+                        </div>
+                        <div>
+                            <ImageDescription {...DOE} priority={true} />
+                        </div>
+                        <div>
+                            <ImageDescription {...DOE} priority={true} />
+                        </div>
+                    </div>
                 </div>
             </section>
         </div>
